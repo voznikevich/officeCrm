673851ebe7f4bb76805612811b5146f3ed5d22a8
voznikevich <voznikevicho@gmail.com>
2024-12-30 18:59:01 +0200

    fixed forex enter price

--- app/helpers/getForexPrice.helper.js
+++ app/helpers/getForexPrice.helper.js
@@ -1,22 +1,28 @@
 const axios = require('axios');
-const doom = require('./doom.helper');
 
-const apiKey = process.env.EXCHANGERATE_API_KEY;
 async function getExchangeRate(fromCurrency, toCurrency) {
-    const url = `https://v6.exchangerate-api.com/v6/${apiKey}/latest/${fromCurrency}`;
+    const apiKey = process.env.ALPHA_API_KEY;
+
+    const url = `https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=${fromCurrency}&to_currency=${toCurrency}&apikey=${apiKey}`;
 
     try {
         const response = await axios.get(url);
 
-        if (response.data && response.data.conversion_rates && response.data.conversion_rates[toCurrency]) {
-            return response.data.conversion_rates[toCurrency];
+        const data = response.data;
+        let price;
+
+        if (data['Realtime Currency Exchange Rate']) {
+            price = data['Realtime Currency Exchange Rate']['5. Exchange Rate'];
+            return price;
         } else {
-            return doom.error.pairNotFound(fromCurrency, toCurrency)
+            console.log(`Дані для ${fromCurrency}/${toCurrency} не знайдено.`);
         }
+
     } catch (error) {
         console.error('Помилка при отриманні курсу:', error.message);
     }
 }
- module.exports = {
+
+module.exports = {
     getExchangeRate
- }
+}
